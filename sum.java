19f4e5a8939982b6c212c234afc1d16f10b614ef
YOGESH SINGH YADAV <86778735+YOGDEV@users.noreply.github.com>
2022-03-05 14:40:00 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,11 @@
+//command line arguments
+class Account {
+
+public static void main(String []arr){
+int x= Integer.parseInt(arr[0]);
+int y= Integer.parseInt(arr[1]);
+System.out.println("Sum Of Two Given Numbers is : "+(x+y));
+}
+
+
+}
